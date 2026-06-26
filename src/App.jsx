1a6e3c1b6f02dd113ae8c7e476d8f6fd427c24ca
scDjanGo <scdjango229101@gmail.com>
2026-06-26 16:10:58 +0600

    add new data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import HeaderMain from './components/header/HeaderMain';
+import HeaderMain from "./components/header/HeaderMain";
 import Homelayout from "./pages/Homelayout";
-import EpisodesPage from "./pages/episodes/page"
-import AboutPage from "./pages/about/page"
-import ContactPage from "./pages/contact/page"
-import HomePage from "./pages/page"
+import EpisodesPage from "./pages/episodes/page";
+import AboutPage from "./pages/about/page";
+import ContactPage from "./pages/contact/page";
+import HomePage from "./pages/page";
 
 function App(props) {
   return (
     <Routes>
-      <Route path="/" element={<Homelayout />} >
-      <Route index element={<HomePage />} />
-      <Route path="episodes" element={<EpisodesPage />} />
-      <Route path="about" element={<AboutPage />} />
-      <Route path="contact" element={<ContactPage />} />
-
-      
-      
+      <Route path="/" element={<Homelayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="episodes" element={<EpisodesPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
       </Route>
     </Routes>
   );
