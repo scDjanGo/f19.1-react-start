e05e664dcf08718b014543651e1633ebec9ad1c2
scDjanGo <scdjango229101@gmail.com>
2026-06-29 10:54:37 +0600

    add

--- src/components/header/HeaderMain.jsx
+++ src/components/header/HeaderMain.jsx
@@ -1,40 +1,63 @@
 import { Link, useNavigate } from "react-router-dom";
 
 function HeaderMain() {
-  const navigate = useNavigate()
-
-
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const {path} = Object.fromEntries(new FormData(e.currentTarget).entries())
-    
-    
-    navigate(`/${path}`)
-    
+    const { path } = Object.fromEntries(
+      new FormData(e.currentTarget).entries(),
+    );
 
-    e.currentTarget.reset()
+    navigate(`/${path}`);
 
+    e.currentTarget.reset();
+  };
 
+  const handlePrev = () => {
+    navigate(-1);
+  };
+
+  const handleNext = () => {
+    navigate(+1);
   }
-
 
   return (
     <header
       className={` flex items-center justify-between gap-3  text-white py-10.75 px-4 bg-black`}
     >
-      <Link to={`/`}>
-        <img src="/svg/logo.svg" alt="" />
-      </Link>
-
-
+      <div className="flex items-center gap-3">
+        <button
+          onClick={handlePrev}
+          to={`/`}
+          className="border rounded-xl p-[6px_12px] text-white"
+        >
+          Prev
+        </button>
+        <button
+          onClick={handleNext}
+          to={`/`}
+          className="border rounded-xl p-[6px_12px] text-white"
+        >
+          Next
+        </button>
+      </div>
 
       <form className="flex gap-3" onSubmit={handleSubmit}>
         <label htmlFor="path">
-          <input type="text" id="path" name="path" placeholder="Write page" required className="border rounded-xl p-[6px_12px] text-white" />
+          <input
+            type="text"
+            id="path"
+            name="path"
+            placeholder="Write page"
+            required
+            className="border rounded-xl p-[6px_12px] text-white"
+          />
         </label>
-        <button className="border rounded-xl p-[6px_12px] text-white">GET</button>
+        <button className="border rounded-xl p-[6px_12px] text-white">
+          GET
+        </button>
       </form>
 
       <nav className="flex items-center gap-10 text-[17px] ">
